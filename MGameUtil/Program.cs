bd7feb4a5eaf73706fdaf394ada1dc364dd8cdef
ET6 <54055724+752636090@users.noreply.github.com>
2021-07-16 10:56:14 +0800

    支持代码打开转换工具

--- MGameUtil/Program.cs
+++ MGameUtil/Program.cs
@@ -7,34 +7,47 @@ namespace MGameUtil
     {
         static void Main(string[] args)
         {
+            string key = "";
+
+            foreach (string item in args)
+            {
+                Thread.Sleep(20);
+                Handle(item);
+            }
+
             while (true)
             {
                 Thread.Sleep(20);
-                string key = Console.ReadLine();
-                if (key == "1")
-                {
-                    UEC2SConverter.Convert();
-                }
-                else if (key == "2")
-                {
-                    AssignListConverter.Convert();
-                }
-                else if (key == "3")
-                {
-                    HandleSet.Handle();
-                }
-                else if (key == "4")
-                {
-                    GenZGlobal.Gen();
-                }
-                else if (key == "5")
-                {
-                    CodeStyleConverter.ConvertToOther();
-                }
-                else if (key == "6")
-                {
-                    CodeStyleConverter.ConvertToMine();
-                }
+                key = Console.ReadLine();
+                Handle(key);
+            }
+        }
+
+        private static void Handle(string key)
+        {
+            if (key == "1")
+            {
+                UEC2SConverter.Convert();
+            }
+            else if (key == "2")
+            {
+                AssignListConverter.Convert();
+            }
+            else if (key == "3")
+            {
+                HandleSet.Handle();
+            }
+            else if (key == "4")
+            {
+                GenZGlobal.Gen();
+            }
+            else if (key == "5")
+            {
+                CodeStyleConverter.ConvertToOther();
+            }
+            else if (key == "6")
+            {
+                CodeStyleConverter.ConvertToMine();
             }
         }
     }
